9be0f6d6c3e4e90aea7f41600bacdc0b4e5599d0
Phil Haines <phil.haines92@gmail.com>
2023-01-12 18:36:40 -0700

    Updates BikeView path

--- src/router/index.js
+++ src/router/index.js
@@ -7,16 +7,13 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'bikes',
+    name: 'BikesView',
     component: BikesView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/motorcycle/:id/:handle',
+    name: 'BikeView',
+    component: () => import(/* webpackChunkName: "BikeView" */ '../views/BikeView.vue')
   }
 ]
 
